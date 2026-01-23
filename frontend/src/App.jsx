bc22c81bd357e6f8ec873d7bd384a7dd99c5e960
egor-morozik <egor.mrz04@gmail.com>
2026-01-23 14:28:07 +0300

    Add routing and connected tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router";
+import { Questions } from "./pages/Questions"
+import { Tests } from "./pages/Tests"
+import { Candidates } from "./pages/Candidates"
+import { Attempts } from "./pages/Attempts"
+import { Results } from "./pages/Results"
+
 function App() {
   return (
-    <h1>
-      Hello world
-    </h1>
-  )
+    <BrowserRouter>
+      <nav>
+        <Link to="/questions">Questions</Link>
+        <Link to="/tests">Tests</Link>
+        <Link to="/candidates">Candidates</Link>
+        <Link to="/attempts">Attempts</Link>
+        <Link to="/results">Results</Link>
+      </nav>
+      <Routes>
+        <Route path="/questions" element={<Questions />} />
+        <Route path="/tests" element={<Tests />} />
+        <Route path="/candidates" element={<Candidates />} />
+        <Route path="/attempts" element={<Attempts />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
