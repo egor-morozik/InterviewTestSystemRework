d236e45a423ccfa9d88b9cc0608993232579d078
egor-morozik <egor.mrz04@gmail.com>
2026-01-26 07:45:42 +0300

    Add send email util

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -128,3 +128,10 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 AUTH_USER_MODEL = "core.User"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "your-email@gmail.com"
+EMAIL_HOST_PASSWORD = "your-app-password"
