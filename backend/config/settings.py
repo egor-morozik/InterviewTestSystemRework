c7f827c56285ce2986879d671edf7a4032b30991
egor-morozik <egor.mrz04@gmail.com>
2026-02-01 21:36:50 +0300

    Add Redis as cache

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -135,3 +135,13 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "your-email@gmail.com"
 EMAIL_HOST_PASSWORD = "your-app-password"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
